609c3747782a85dfbc92744cc30f98f8dd987de7
shun-okamoto <okamons8811@gmail.com>
2023-05-07 19:02:12 +0900

    一番簡単なテスト

--- unittest/src/03/02/index.test.ts
+++ unittest/src/03/02/index.test.ts
@@ -1,20 +1,26 @@
-import { add, sub } from ".";
+import { add, sub } from '.'
 
-describe("四則演算", () => {
-  describe("add", () => {
-    test("1 + 1 は 2", () => {
-      expect(add(1, 1)).toBe(2);
-    });
-    test("1 + 2 は 3", () => {
-      expect(add(1, 2)).toBe(3);
-    });
-  });
-  describe("sub", () => {
-    test("1 - 1 は 0", () => {
-      expect(sub(1, 1)).toBe(0);
-    });
-    test("2 - 1 は 1", () => {
-      expect(sub(2, 1)).toBe(1);
-    });
-  });
-});
+// 単数
+test('1 + 1 は 2', () => {
+  expect(add(1, 1)).toBe(2)
+})
+
+// describe('テストタイトル', (関数) => {})を使用することでグループ分けできる。またネストも可能
+describe('四則演算', () => {
+  describe('add', () => {
+    test('1 + 1 は 2', () => {
+      expect(add(1, 1)).toBe(2)
+    })
+    test('1 + 2 は 3', () => {
+      expect(add(1, 2)).toBe(3)
+    })
+  })
+  describe('sub', () => {
+    test('1 - 1 は 0', () => {
+      expect(sub(1, 1)).toBe(0)
+    })
+    test('2 - 1 は 1', () => {
+      expect(sub(2, 1)).toBe(1)
+    })
+  })
+})
